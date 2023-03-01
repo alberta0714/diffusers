dd13a4b62c22a2df73f394c367ad578a706c6ba1
sunzhanchao <sunzhanchao@igetcool.com>
2023-03-01 11:49:40 +0800

    ac

--- examples/example.sh
+++ examples/example.sh
@@ -1,2 +1,9 @@
-python3 examples/text_to_image_sd2.py
 vim examples/text_to_image_sd2.py
+
+python3 examples/text_to_image_sd2.py
+
+scp root@152.136.186.81:/usr/local/code/onediff/cat_2048.png ./
+
+033227
+033458
+02:31 2分31秒
