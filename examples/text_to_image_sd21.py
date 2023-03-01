2d99bb21bfba3c3bca42cf21b4c08fed6aec7600
sunzhanchao <sunzhanchao@igetcool.com>
2023-03-01 15:12:52 +0800

    ac

--- examples/text_to_image_sd21.py
+++ examples/text_to_image_sd21.py
@@ -17,7 +17,7 @@ pipe = pipe.to("cuda")
 print(strftime("%Y-%m-%d %H:%M:%S", gmtime()))
 #prompt = "a photo of an astronaut riding a horse on mars"
 #image = pipe(prompt, height=768, width=768).images[0]
-prompt = "A warm hut."
+prompt = "A warm hut in the night"
 # image = pipe(prompt, height=2048, width=2048).images[0]
 image = pipe(prompt, height=768, width=768).images[0]
 print(strftime("%Y-%m-%d %H:%M:%S", gmtime()))
