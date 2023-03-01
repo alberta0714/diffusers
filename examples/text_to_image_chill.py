445b4c3a5755e939d225a683f672cf8e7ab25787
sunzhanchao <sunzhanchao@igetcool.com>
2023-03-01 14:35:35 +0800

    ac

---
+++ examples/text_to_image_chill.py
@@ -0,0 +1,26 @@
+import oneflow as flow
+from time import gmtime, strftime
+flow.mock_torch.enable()
+
+from diffusers import EulerDiscreteScheduler
+from onediff import OneFlowStableDiffusionPipeline
+
+print(strftime("%Y-%m-%d %H:%M:%S", gmtime()))
+model_id = "swl-models/chilloutmix"
+# Use the Euler scheduler here instead
+scheduler = EulerDiscreteScheduler.from_pretrained(model_id, subfolder="scheduler")
+pipe = OneFlowStableDiffusionPipeline.from_pretrained(
+    model_id, scheduler=scheduler, revision="fp16", torch_dtype=flow.float16
+)
+pipe = pipe.to("cuda")
+
+print(strftime("%Y-%m-%d %H:%M:%S", gmtime()))
+#prompt = "a photo of an astronaut riding a horse on mars"
+#image = pipe(prompt, height=768, width=768).images[0]
+prompt = "cat"
+# image = pipe(prompt, height=2048, width=2048).images[0]
+image = pipe(prompt, height=768, width=768).images[0]
+print(strftime("%Y-%m-%d %H:%M:%S", gmtime()))
+
+image.save("cat_2_1_768.png")
+print(strftime("%Y-%m-%d %H:%M:%S", gmtime()))
--- push.sh
+++ push.sh
@@ -5,7 +5,8 @@ git push
 SSHREMOTE=root@152.136.186.81
 
 ssh ${SSHREMOTE} 'cd /usr/local/code/onediff; git pull'
-ssh ${SSHREMOTE} 'cd /usr/local/code/onediff; python3 examples/text_to_image_sd21.py'
+#ssh ${SSHREMOTE} 'cd /usr/local/code/onediff; python3 examples/text_to_image_sd21.py'
+ssh ${SSHREMOTE} 'cd /usr/local/code/onediff; python3 examples/text_to_image_chill.py'
 
 scp ${SSHREMOTE}:/usr/local/code/onediff/cat_2_1_768.png /Users/sunzhanchao/stabilityai/
 
