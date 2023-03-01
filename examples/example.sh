6ee701beb15213159c6c58cebbda26679eaf19d6
sunzhanchao <sunzhanchao@igetcool.com>
2023-03-01 13:58:24 +0800

    ac

--- examples/example.sh
+++ examples/example.sh
@@ -3,6 +3,7 @@ vim examples/text_to_image_sd2.py
 python3 examples/text_to_image_sd2.py
 
 scp root@152.136.186.81:/usr/local/code/onediff/cat_2048.png ./
+scp root@152.136.186.81:/usr/local/code/onediff/cat.png ./
 
 033227
 033458
